5b89e730b012dce36ac7181d966a4770f77f0ce2
Ivan Fuentes <ivanfuentesglez@gmail.com>
2021-08-14 11:17:05 -0500

    Estructura inicial del proyecto

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function () {
+  new fullpage("#fullpage", {
+    navigation: true,
+    responsiveWidth: 700,
+    anchors: ["home", "about-us", "contact"],
+    parallax: true,
+    onLeave: function (origin, destination, direction) {
+      console.log("Leaving section" + origin.index);
+    },
+  });
+})();
